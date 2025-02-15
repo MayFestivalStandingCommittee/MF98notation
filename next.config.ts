34c558ff3941684d5705864f538164b5bbb0df7e
mpses <nsorangepv@gmail.com>
2025-02-16 01:06:17 +0900

    [feat] basePath の設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const basePath = process.env.BASE_PATH || '';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath,
 };
 
 export default nextConfig;
